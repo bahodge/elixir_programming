3bef753022048016bc60cc3556e4e606a81eb0c2
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2019-01-28 08:55:01 -0600

    working on thinggies

--- elixir_for_beginners/cook.exs
+++ elixir_for_beginners/cook.exs
@@ -1,6 +1,16 @@
 defmodule Cook do
-  def boil(food) do
-    f = "Boil #{food}"
-    String.upcase(f)
+
+  # Public Function
+  def cook(meat, veg), do: _cook("Grill", meat) <> _cook("Boil", veg)
+
+  # Private function
+  defp _cook(cook, food) do
+    "#{cook} #{food} "
   end
+
 end
+
+# IO.inspect Cook.boil("cookies")
+# IO.inspect Cook.sausage
+IO.inspect Cook.cook("bacon", "pasta")
+# IO.inspect Cook._cook "pasta", "bacon"
