bba10901beba713acc29c5ce664b015f27a320a1
benhodge <benjamin.andrew.hodge@gmail.com>
2019-02-06 23:16:58 -0600

    working on controllers/ fixed view

--- elixir_bootcamp/discuss/web/controllers/topic_controller.ex
+++ elixir_bootcamp/discuss/web/controllers/topic_controller.ex
@@ -7,6 +7,7 @@ defmodule Discuss.TopicController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, params) do
+  def create(conn, %{"topic" => topic}) do
+    IO.inspect(params)
   end
 end
