859d6b9d1be25369dcb35f862f63499cd019ce5a
benhodge <benjamin.andrew.hodge@gmail.com>
2019-02-20 23:45:53 -0600

    working on implementing web sockets

--- elixir_bootcamp/discuss/web/controllers/topic_controller.ex
+++ elixir_bootcamp/discuss/web/controllers/topic_controller.ex
@@ -2,12 +2,10 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  plug  Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
-  plug :check_topic_owner when action in [:update, :edit, :delete]
-
+  plug(Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete])
+  plug(:check_topic_owner when action in [:update, :edit, :delete])
 
   def index(conn, _params) do
-
     topics = Repo.all(Topic)
 
     render(conn, "index.html", topics: topics)
@@ -18,17 +16,16 @@ defmodule Discuss.TopicController do
     render(conn, "show.html", topic: topic)
   end
 
-
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
-
   def create(conn, %{"topic" => topic}) do
-    changeset = conn.assigns.user
-                |> build_assoc(:topics)
-                |> Topic.changeset(topic)
+    changeset =
+      conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -77,12 +74,13 @@ defmodule Discuss.TopicController do
         "id" => topic_id
       }
     } = conn
+
     if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
       conn
     else
       conn
       |> put_flash(:error, "You cant do that")
-      |> redirect(to: topic_path(conn,  :index))
+      |> redirect(to: topic_path(conn, :index))
       |> halt()
     end
   end
